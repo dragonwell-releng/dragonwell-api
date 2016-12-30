7357b29879db12dece30ccc0a8960e783bdc34c1
wenqian.peiwq <wenqian.peiwq@alibaba-inc.com>
2016-12-30 14:16:27 +0800

    add JWarmUp API

---
+++ src/com/alibaba/jwarmup/JWarmUp.java
@@ -0,0 +1,33 @@
+package com.alibaba.jwarmup;
+
+/**
+ * If the CompilationWarmUpOptimistic is off, application should
+ * implicitly notify jvm that startup of appliacation is done
+ */
+public class JWarmUp {
+    /**
+     * Notify jvm that application startup is done.
+     * <p>
+     * Should be explicitly call after startup of application if
+     * CompilationWarmUpOptimistic is off. Otherwise, it does
+     * nothing and just prints a warning message.
+     *
+     * @version 1.8
+     */
+    public static synchronized void notifyApplicationStartUpIsDone() {
+        throw new RuntimeException("AJDK dummy API found");
+    }
+
+    /**
+     * Check if the last compilation submitted by JWarmUp is complete.
+     * <p>
+     * call this method after <code>notifyApplicationStartUpIsDone</code>
+     *
+     * @return true if the last compilation task is complete.
+     *
+     * @version 1.8
+     */
+    public static synchronized boolean checkIfCompilationIsComplete() {
+        throw new RuntimeException("AJDK dummy API found");
+    }
+}
